f7495756d70f6ef1abc7949901ebd0ee7708b5cf
Afolabi Sunday <sundayafolabi992@gmail.com>
2023-01-21 01:43:14 +0000

    updated code

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ class StringCalculator:
     def __init__(self) -> None:
         pass
 
+
+    def custom_delimiter(self, numbers:str):
+        '''
+        create a custom delimiter to handle all custom delimeters
+        '''
+        if numbers[:2] == "//":  # checking for delimiter setter
+            num = numbers.replace("//", '').replace('\n', '') #replaced both the // and new line with an empty string
+            patterns = re.compile(r'[^***%;\[\]]')  #using regex pattern to check for special character used as delimiter for the numbers
+            p_nums = patterns.finditer(num) #using finditer helps to comb through the string and return what matches the pattern
+            list_number = [] 
+            for i in p_nums: #loop through our result and append it to the empty list created above
+                list_number.append(int(i.group(0)))
+            return list_number
+
     def Add(self, numbers):
         if numbers == "":  # checking for empty input numbers
             return 0
@@ -13,26 +27,19 @@ class StringCalculator:
             # if number is valid and not empty
             delimiter = ","  # setting the delimiter for separating the input values
             if numbers[:2] == "//":  # checking for delimiter setter
-                _delimiter = re.search(
-                    "(?<=\[).*(?=\])", numbers )  # using regular expression to search through the input values and group it, creating the custom delimiter
+                empty_numbers = self.custom_delimiter(numbers)
+
+            else:
+                mod_numbers = re.sub(delimiter, ",", numbers) 
+                # the above line of code is using the sub() function from the 
+                # re module to replace all occurrences of the delimiter variable with a "," 
+                # in the numbers variable and returns the modified string.
+                #using re sub method we are able to check for
                 
-                delimiter = _delimiter.group()
-                numbers = numbers[numbers.index("\n") + 1 :]
-                print(numbers)
-                # starting at the index one after the first instance of the new line (string "\n")
-                # the index() helps us find the first instance of \n and we add 1, 
-                # this should include all character after the new line
+                replaced_numbers = mod_numbers.replace("\n", ",")
+                #the above code replace all occupance of the newline
 
-            mod_numbers = re.sub(delimiter, ",", numbers) 
-            # the above line of code is using the sub() function from the 
-            # re module to replace all occurrences of the delimiter variable with a "," 
-            # in the numbers variable and returns the modified string.
-            #using re sub method we are able to check for
-            
-            replaced_numbers = mod_numbers.replace("\n", ",")
-            #the above code replace all occupance of the newline
-
-            empty_numbers = [int(x) for x in replaced_numbers.split(",") if x != ""]
+                empty_numbers = [int(x) for x in replaced_numbers.split(",") if x != ""]
 
             negatives = [x for x in empty_numbers if x < 0] #using list compherention to check for negative values
 
